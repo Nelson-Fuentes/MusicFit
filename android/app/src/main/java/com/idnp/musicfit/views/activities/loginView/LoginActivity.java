8073279581f467f89d271a27a30216b69ed80c5c
Nelson-Fuentes <40514220+Nelson-Fuentes@users.noreply.github.com>
2020-11-04 01:23:52 -0500

    Login View finished

--- android/app/src/main/java/com/idnp/musicfit/views/activities/loginView/LoginActivity.java
+++ android/app/src/main/java/com/idnp/musicfit/views/activities/loginView/LoginActivity.java
@@ -4,8 +4,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.TextView;
 
 import com.idnp.musicfit.R;
 import com.idnp.musicfit.models.services.authenticationService.AuthenticationConstant;
@@ -26,6 +32,7 @@ public class LoginActivity extends AppCompatActivity implements iLoginView{
         setContentView(R.layout.activity_login);
         this.getSupportActionBar().hide();
         this.loginPresenter = new LoginPresenter(this);
+        this.loadComponents();
     }
 
     public void startRegisterActivity(View view){
@@ -56,6 +63,35 @@ public class LoginActivity extends AppCompatActivity implements iLoginView{
         Intent intent = new Intent(this, MainActivity.class);
         this.startActivity(intent);
         this.finish();
+    }
+
+    public void loadComponents(){
+        TextView textView = (TextView) this.findViewById(R.id.go_to_register_edit_text);
+        String goRegister = (String) textView.getText();
+        String registerString = (String) this.getString(R.string.register_label);
+        int start_first = 0, last_first = goRegister.indexOf(registerString);
+        int last_second = last_first + registerString.length();
+        int last_third = goRegister.length();
+
+        System.out.println(start_first + " -------------------" + last_first + "------------" + last_second  + "--------------" + last_third);
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+
+        SpannableString str1= new SpannableString(goRegister.substring(start_first, last_first));
+        str1.setSpan(new ForegroundColorSpan(getColor(R.color.black)), 0, str1.length(), 0);
+        builder.append(str1);
+
+        SpannableString str2= new SpannableString(goRegister.substring(last_first, last_second));
+        str2.setSpan(new ForegroundColorSpan(getColor(R.color.main_color)), 0, str2.length(), 0);
+        builder.append(str2);
+
+        SpannableString str3= new SpannableString(goRegister.substring(last_second, last_third));
+        str3.setSpan(new ForegroundColorSpan(getColor(R.color.black)), 0, str3.length(), 0);
+        builder.append(str3);
+
+        textView.setText( builder, TextView.BufferType.SPANNABLE);
+
+//        String color = this.getResources().getString(R.color.main_color);
+//        textView.setText(Html.fromHtml(goRegister), TextView.BufferType.SPANNABLE);
     }
 
     @Override
