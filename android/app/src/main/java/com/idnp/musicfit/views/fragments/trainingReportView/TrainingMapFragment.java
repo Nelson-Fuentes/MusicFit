35814932f6640233156ee6c759d01cb156665a8d
KelvinVPLuser <kticllahuanaco@usnsa.edu.pe>
2020-11-12 02:02:15 -0500

    se agregaron algunos cambios en el apartado de reporte Map

--- android/app/src/main/java/com/idnp/musicfit/views/fragments/trainingReportView/TrainingMapFragment.java
+++ android/app/src/main/java/com/idnp/musicfit/views/fragments/trainingReportView/TrainingMapFragment.java
@@ -2,23 +2,49 @@ package com.idnp.musicfit.views.fragments.trainingReportView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 import com.idnp.musicfit.R;
 
-public class TrainingMapFragment extends Fragment {
+import com.idnp.musicfit.views.toastManager.ToastManager;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class TrainingMapFragment extends Fragment {
+    GoogleMap map;
+    Boolean myPosition = true;
+    JSONObject jso;
+    Double longOri, latOri;
+    ToastManager mytoast;
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -30,13 +56,119 @@ public class TrainingMapFragment extends Fragment {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            map = googleMap;
+            //PARA LOS PERMISOS DE ACCESO
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)){
+                }else{
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
+                }
+                if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)){
+                }else{
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                }
+                return;
+            }
+
+            //PARA OBTENER MI LOCALIZACIÓN
+            map.setMyLocationEnabled(true);
+            map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location location) {
+                    if(myPosition){
+                        latOri=location.getLatitude();
+                        longOri=location.getLongitude();
+                        myPosition=false;
+
+                        mytoast= new ToastManager(getContext());
+
+                        mytoast.showToast("("+latOri+","+googleMap.getMapType()+")");
+                        //para graficar esta posición en el mapa
+                        LatLng myPosition = new LatLng(latOri, longOri);
+
+                        googleMap.addMarker(new MarkerOptions().position(myPosition).title("Esta es mi posición"));
+                        CameraPosition cameraPosition= new CameraPosition.Builder()
+                                .target(new LatLng(latOri,longOri))
+                                .zoom(15)
+                                .bearing(30)
+                                .build();
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+
+                        //para obtener el JSON de los datos de ruta
+                        String url="https://maps.googleapis.com/maps/api/directions/json?origin="+latOri+","+longOri+"&destination="+-16.4322583+","+-71.5642415;
+                        RequestQueue queue= Volley.newRequestQueue(getActivity());
+                        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    //aqui ya obtenemos la respuesta a la consulta de la "url" y lo ponemos en el jso
+                                    jso=new JSONObject(response);
+                                    trazarRuta(jso);
+                                    googleMap.addMarker(new MarkerOptions().position(new LatLng(-16.4322583,-71.5642415)).title("Esta es mi posición"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                        queue.add(stringRequest);
+
+                    }
+                }
+            });
+
         }
     };
+
+    private void trazarRuta(JSONObject jso) {
+        JSONArray jRoutes;
+        JSONArray jLegs;
+        JSONArray jSteps;
+        try{
+            jRoutes=jso.getJSONArray("routes");
+            for(int i=0;i<jRoutes.length();i++){
+                jLegs=((JSONObject)(jRoutes.get(i))).getJSONArray("legs");
+                for(int j=0;j<jLegs.length();j++){
+                    jSteps=((JSONObject)jLegs.get(j)).getJSONArray("steps");
+                    for (int k=0;k<jSteps.length();k++){
+                        String polyline=""+((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                        List<LatLng>list= PolyUtil.decode(polyline);
+                        map.addPolyline(new PolylineOptions().addAll(list).color(Color.GRAY).width(6));
+                    }
+                }
+            }
+        }catch (JSONException e){
+            mytoast.showToast(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String perissions[],int[] grantResults){
+        switch (requestCode){
+            case 1:{
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                }else{
+
+                }
+                return;
+            }
+        }
+    }
 
     @Nullable
     @Override
